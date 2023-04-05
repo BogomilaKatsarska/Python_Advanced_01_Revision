25b26f68ed346c6a1fd59538db82ad3634835247
Bogomila <bkatsarska@abv.bg>
2023-04-05 12:26:59 +0300

    15 commit

--- Python_OOP_02_Revision/Design_Patterns_11.py
+++ Python_OOP_02_Revision/Design_Patterns_11.py
@@ -1,9 +1,24 @@
 '''
-0:44
 refactoring.guru/design-patterns
 
-- Creational patterns
-- Structural patterns
-- Behavioral patterns
+1.- Creational patterns
+2.- Structural patterns
+3.- Behavioral patterns
 
+1.
+pip install structlog
+
+@singleton/creational/ - /useful for logs/ - ensure a class only has one instance and provide a global point of access for it
+abstract factory/creational/ - provides an interface for creating families of related or dependent objects without specifying their concrete class
+factory method/creational/  - define an interface for creating an object, but let subclasses decide which class to instantiate.
+Let a class defer instantiation to subclasses
+builder/creational/ - separate the construction of a complex object from its representing so that the same construction process
+can create different representations.
+
+2.
+facade - cooker example
+decorator pattern
+
+3.
+pass
 '''
